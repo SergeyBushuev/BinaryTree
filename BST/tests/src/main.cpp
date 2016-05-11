f0e2db7641cc767ae6d94535b2eb963ba042365b
Varvar <Sergeybushuev14@gmail.com>
2016-05-11 08:36:33 +0300

    Update main.cpp

--- BST/tests/src/main.cpp
+++ BST/tests/src/main.cpp
@@ -1,1 +1,16 @@
 
+#include <node.h>
+
+#include <catch.hpp>
+#include <fstream>
+#include <iostream>
+
+SCENARIO("insert_int", "[insert_node]") {
+	Tree<int> intTree;
+	REQUIRE(intTree.insert_node(1));
+	REQUIRE(intTree.insert_node(2));
+	REQUIRE(intTree.insert_node(3));
+	REQUIRE(intTree.find_node(2));
+	REQUIRE(intTree.find_node(3));
+	REQUIRE(intTree.find_node(1));
+}
