54f088c55897a4b6816401fb84d0582d0adf1031
Varvar <Sergeybushuev14@gmail.com>
2016-05-17 20:48:06 +0300

    Update Tree.cpp

--- Include/Tree.cpp
+++ Include/Tree.cpp
@@ -4,22 +4,19 @@
 
 template <class T> class Tree;
 using namespace std;
-template <class T>
-class Tree
+
+template<class T>
+TreeNode <T>::TreeNode(const T &a)
 {
-public:
-	Tree();                                                    // Конструктор  
-	int insert_node(const T &);                         // Вставляет узел  
-	TreeNode<T>* delete_node(TreeNode<T> *);     // Удаляет узел  
-	void inorder_walk(TreeNode<T>*);                    // Печатает все ключи в неубывающем порядке  
-	TreeNode<T>* find_max(TreeNode<T>*);         // Находит узел с минимальным значением ключа и возвращает указатель на него  
-	TreeNode<T>* find_min(TreeNode<T>*);
-	TreeNode<T>* find_node(TreeNode<T>*, const T &);  // Находит элемент по ключу
-	TreeNode<T>* find_succsessor(const T &);            // Находит следубщий за элементом с данным ключом элемент
-	TreeNode<T> *get_root();                            // Возвращает указатель на корень дерева  
-private:
-	TreeNode<T> *root;                                  // Корень  
-};
+	key = a;
+	left = 0;
+	right = 0;
+}
+template <class T>
+T TreeNode <T>::get_key()
+{
+	return key;
+}
 
 template<class T>
 Tree<T>::Tree()
